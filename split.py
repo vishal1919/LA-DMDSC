2bb33a8885c50df97952c9125a7017a4300a0dab
VISHAL <64350975+vishal1919@users.noreply.github.com>
2026-05-18 10:50:02 +0530

    Update split.py

--- split.py
+++ split.py
@@ -2,9 +2,9 @@ splits_2020 = {
 
     'bloodmnist': [
         {'known': [6, 1, 3, 7], 'unknown': [0, 2, 4, 5]},
-        {'known': [1, 6, 7, 2], 'unknown': [4, 3, 0, 5]},
+        {'known': [7, 2, 1, 6], 'unknown': [4, 3, 0, 5]},
         {'known': [2, 3, 6, 1], 'unknown': [0, 7, 4, 5]},
-        {'known': [7, 1, 2, 3], 'unknown': [5, 4, 6, 0]},
+        {'known': [3, 1, 2, 7], 'unknown': [5, 4, 6, 0]},
         {'known': [0, 7, 4, 6], 'unknown': [1, 3, 5, 2]}
     ],
     'breakhis_40': [
